201a1ac5b446656d9b4fa6f639e2e3b967d5e978
paulilie <i.paul21@yahoo.com>
2020-05-07 00:35:16 +0300

    laborator 5.7.2020

---
+++ Asp_Ilie_Paul_Rp/Asp_Ilie_Paul_Rp/Data/Asp_Ilie_Paul_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Ilie_Paul_Rp.Models;
+
+namespace Asp_Ilie_Paul_Rp.Data
+{
+    public class Asp_Ilie_Paul_RpContext : DbContext
+    {
+        public Asp_Ilie_Paul_RpContext (DbContextOptions<Asp_Ilie_Paul_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Ilie_Paul_Rp.Models.Movie> Movie { get; set; }
+    }
+}
